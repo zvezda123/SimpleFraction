d4f53d09439120f9c64ea87c7f8a5316494df809
zvezda <alyciabelkessam123@gmail.com>
2025-09-18 00:04:45 +0200

    commit 1: ajouter class fraction vide et main qui affiche hello git

---
+++ SimpleFraction/src/td1/FRACTION.java
@@ -0,0 +1,5 @@
+package td1;
+
+public class FRACTION {
+
+}
---
+++ SimpleFraction/src/td1/MAIN.java
@@ -0,0 +1,10 @@
+package td1;
+
+public class MAIN {
+
+	public static void main(String[] args) {
+		System.out.println("hello git");
+
+	}
+
+}
